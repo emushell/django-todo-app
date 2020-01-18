b4e727d98cccb9fc9ad340c317d4aa7764b96cac
artustra <arturs.strauss@gmail.com>
2020-01-18 13:00:08 +0800

    Creating decorators - for controlling access to views, based on user role permissions.

--- todo/views.py
+++ todo/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render, redirect
 from .models import Task
 from .forms import TaskForm
 
+from django.contrib.auth.models import Group
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
+from .decorators import unauthenticated_user, allowed_users
 
 
 # Create your views here.
-
+@login_required(login_url='login')
 def index(request):
     tasks = Task.objects.all()
     form = TaskForm()
@@ -17,14 +20,13 @@ def index(request):
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, "Account was created for " + user)
         return redirect('index')
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'todo/index.html', context)
 
 
+@login_required(login_url='login')
 def update_task(request, task_id):
     task = Task.objects.get(id=task_id)
     form = TaskForm(instance=task)
@@ -39,6 +41,8 @@ def update_task(request, task_id):
     return render(request, 'todo/update_task.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def delete_task(request, task_id):
     item = Task.objects.get(id=task_id)
 
@@ -51,19 +55,26 @@ def delete_task(request, task_id):
     return render(request, 'todo/delete_task.html', context)
 
 
+@unauthenticated_user
 def register_page(request):
     form = CreateUserForm()
-
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='default_user')
+            user.groups.add(group)
+
+            messages.success(request, "Account was created for " + username)
             return redirect('login')
 
     context = {'form': form}
     return render(request, 'todo/register.html', context)
 
 
+@unauthenticated_user
 def login_page(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -79,6 +90,7 @@ def login_page(request):
     return render(request, 'todo/login.html', context)
 
 
+@login_required(login_url='login')
 def logout_user(request):
     logout(request)
     return redirect('login')
