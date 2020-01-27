c8d5f0a58941079353104ab601c3eabdcb512fc4
artustra <arturs.strauss@gmail.com>
2020-01-27 23:17:32 +0800

    Allowed hosts

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emushell-todo-app.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
